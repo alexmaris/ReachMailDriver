b60033200c29039c13a2febdd1954fdcfd90b2ab
Alex Maris <alexandru.maris@gmail.com>
2015-07-29 06:17:00 -0700

    Removing key...

--- ReachMailDriver/Program.cs
+++ ReachMailDriver/Program.cs
@@ -6,7 +6,7 @@ namespace ReachMailDriver
     class Program
     {
         //TODO: hide this secret, don't share
-        private readonly static String apiKey = "pApaxtFpKUEWzpE82-P9s-GIeZRcdhPOp7uS8JKr0FKWjwA1HmEGvz6TjAT8bsQ2";
+        private readonly static String apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
         private readonly static List<String> emails = new List<string>{
             "alexandru.maris@gmail.com",
